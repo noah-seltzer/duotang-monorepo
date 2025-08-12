49df7dae161898190da85041f92ac5e2a1233e9f
Noah Seltzer <noahseltzer@outlook.com>
2025-08-12 02:12:46 -0700

    feat(client):add redux persist with localforage

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
-import fileListSlice  from './fileListSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import localForage from 'localforage'
+import {
+      FLUSH,
+      REHYDRATE,
+      PAUSE,
+      PERSIST,
+      PURGE,
+      REGISTER,
+      persistStore,
+      persistReducer
+    } from 'redux-persist';
+import fileListSlice from './fileListSlice'
 import clientInfoSlice from './clientInfoSlice'
 
-export const store = configureStore({
-  reducer: {
+const rootReducer = combineReducers({
     fileList: fileListSlice,
     clientInfo: clientInfoSlice
-  },
 })
+
+const persistConfig = {
+  key: 'root',
+  storage: localForage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+})
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
